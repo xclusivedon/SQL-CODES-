d5b82a9330228841c7fd243084d6d2797e4b56e8
Tunde Ajose-Ismail <80249869+xclusivedon@users.noreply.github.com>
2023-07-17 19:30:47 -0400

    Update SCALAR+SUBQUERIES.sql

--- SCALAR+SUBQUERIES.sql
+++ SCALAR+SUBQUERIES.sql
@@ -1,9 +1,9 @@
 --Exercise 1
 /* Adding a derived column called "MaxVacationHours" by the use of a subquery that returns the maximum amount of vacation hours for any one employee, in any given row.*/
 
-SELECT BusinessEntityID,
+SELECT     BusinessEntityID,
 	   JobTitle,
-       VacationHours,
+           VacationHours,
 	   [MaxVacationHours] = (SELECT MAX(VacationHours) FROM AdventureWorks2022.HumanResources.Employee)
 
 FROM AdventureWorks2022.HumanResources.Employee
@@ -12,9 +12,9 @@ FROM AdventureWorks2022.HumanResources.Employee
 --Exercise 2
 /* Returns the percent an individual employees' vacation hours of the maximum vacation hours for any employee */
 
-SELECT BusinessEntityID,
+SELECT     BusinessEntityID,
 	   JobTitle,
-       VacationHours,
+          VacationHours,
 	   [MaxVacationHours] = (SELECT MAX(VacationHours) FROM AdventureWorks2022.HumanResources.Employee),
 	   [PercentVacationHours] =  (1.0 * VacationHours) / (SELECT MAX(VacationHours) FROM AdventureWorks2022.HumanResources.Employee)
 
@@ -26,9 +26,9 @@ FROM AdventureWorks2022.HumanResources.Employee
 /* Refine your output with a criterion in the WHERE clause 
 that filters out any employees whose vacation hours are less then 80% of the maximum amount of vacation hours */
 
-SELECT BusinessEntityID,
+SELECT     BusinessEntityID,
 	   JobTitle,
-       VacationHours,
+           VacationHours,
 	   [MaxVacationHours] = (SELECT MAX(VacationHours) FROM AdventureWorks2022.HumanResources.Employee),
 	   [PercentVacationHours] =  (1.0 * VacationHours) / (SELECT MAX(VacationHours) FROM AdventureWorks2022.HumanResources.Employee)
 	   
